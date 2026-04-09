2ba952b9344fe6d3fc169cfbc6054f12723f451e
Sujayreddy7 <psujay511@gmail.com>
2026-04-10 00:04:17 +0530

    Update frontend Render URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://studygenie-backend-8uvy.onrender.com/api/',
 });
 
 // Axios Interceptor to attach Authorization header
